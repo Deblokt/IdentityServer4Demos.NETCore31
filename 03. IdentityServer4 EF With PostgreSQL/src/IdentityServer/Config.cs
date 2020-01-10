33e34156a017a3275155ebdecc9472f28c049445
JovanBabic <jovan.babic@deblokt.com>
2020-01-10 16:36:39 +0100

    03. IdentityServer4 EF with PostGreSQL - inital commit

---
+++ 03. IdentityServer4 EF With PostgreSQL/src/IdentityServer/Config.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            { };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { };
+        
+    }
+}
