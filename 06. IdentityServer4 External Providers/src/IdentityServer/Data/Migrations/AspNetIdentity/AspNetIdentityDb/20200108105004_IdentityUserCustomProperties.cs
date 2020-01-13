9ae1d9137746f61ba6ceae39c118c96714fb673a
JovanBabic <jovan.babic@deblokt.com>
2020-01-13 11:08:42 +0100

    06 IdentityServer4 External Providers - initial commit

---
+++ 06. IdentityServer4 External Providers/src/IdentityServer/Data/Migrations/AspNetIdentity/AspNetIdentityDb/20200108105004_IdentityUserCustomProperties.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Data.Migrations.AspNetIdentity.AspNetIdentityDb
+{
+    public partial class IdentityUserCustomProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "ApiResource",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 80, DateTimeKind.Utc).AddTicks(4644));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "68211baf-8c99-4559-b6a9-3e75026c2186", "AQAAAAEAACcQAAAAEFOURCmvRZGFwfCY6T/Gx/syQasTZYyNJpIeQQm/z0M/L56NlhlygW0g2dbdaVAqZQ==", "85e246c1-161b-48e7-a907-4baba8279459" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7520ec33-b5e7-44ed-aa19-ca6fb3ffba36", "AQAAAAEAACcQAAAAEH7+MMsOELTd5tLbJG4I3HN3tXTsdmZV732W/3RJlWMyA+FpuBvapGublTE0MIWGpg==", "3b0cc69f-be9a-449c-9100-b56af88ba38a" });
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 100, DateTimeKind.Utc).AddTicks(5049));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 100, DateTimeKind.Utc).AddTicks(8644));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 100, DateTimeKind.Utc).AddTicks(8649));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 100, DateTimeKind.Utc).AddTicks(8651));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 101, DateTimeKind.Utc).AddTicks(2278));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 103, DateTimeKind.Utc).AddTicks(5080));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 103, DateTimeKind.Utc).AddTicks(5327));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 68, DateTimeKind.Utc).AddTicks(4174));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 68, DateTimeKind.Utc).AddTicks(5139));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 50, 4, 68, DateTimeKind.Utc).AddTicks(5142));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "ApiResource",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 872, DateTimeKind.Utc).AddTicks(3424));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7d6fc3cb-ac89-4734-a12d-1dd7783d3810", "AQAAAAEAACcQAAAAEDByjnfz3toa+nri/qd7n6nvHIUzuDGVbxZQEzhmm0aFPLjhIjfgQ5bJ1ZAmu9zH/w==", "d88cda1d-b6fb-4917-97db-274a7e8a488d" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7fee7497-c27c-47b7-bc2f-9cf1ae1439fc", "AQAAAAEAACcQAAAAECC8b7byXubnqvPmFuxlih266CVOZ1NGtrfh+iCoEQoVgLvr8UzZvOj3+W9EMOsJpw==", "f37f2bbe-1de3-4d64-bfef-1e1e74d13c33" });
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 893, DateTimeKind.Utc).AddTicks(1941));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 893, DateTimeKind.Utc).AddTicks(5422));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 893, DateTimeKind.Utc).AddTicks(5428));
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 893, DateTimeKind.Utc).AddTicks(5429));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 893, DateTimeKind.Utc).AddTicks(9167));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 896, DateTimeKind.Utc).AddTicks(2159));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecret",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 896, DateTimeKind.Utc).AddTicks(2403));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 860, DateTimeKind.Utc).AddTicks(2267));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 860, DateTimeKind.Utc).AddTicks(3244));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResource",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 1, 8, 10, 21, 19, 860, DateTimeKind.Utc).AddTicks(3247));
+        }
+    }
+}
---
+++ 06. IdentityServer4 External Providers/src/IdentityServer/Startup.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using IdentityServer4.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using IdentityServer.Data;
+using IdentityServer4.Services;
+using IdentityServer.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Okta.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            //var builder = services.AddIdentityServer()
+            //    .AddInMemoryIdentityResources(Config.Ids)
+            //    .AddInMemoryApiResources(Config.Apis)
+            //    .AddInMemoryClients(Config.Clients);
+
+            services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly)));
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication()
+                .AddAzureAD(options => Configuration.Bind("AzureAd", options))
+                .AddOktaMvc(new OktaMvcOptions
+                 {
+                     // Replace these values with your Okta configuration
+                     OktaDomain = Configuration.GetSection("Okta").GetValue<string>("Authority"),
+                     ClientId = Configuration.GetSection("Okta").GetValue<string>("ClientId"),
+                     Scope = new List<string> { "openid", "profile", "email" },
+                     CallbackPath = Configuration.GetSection("Okta").GetValue<string>("CallbackPath"),
+                     ClientSecret = Configuration.GetSection("Okta").GetValue<string>("Secret")
+                 });
+
+            services.Configure<AzureADOptions>(Configuration.GetSection("AzureAd"));
+
+            services.Configure<OpenIdConnectOptions>("okta", options =>
+            {
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+            });
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.UserInteraction.LoginUrl = "/Account/Login";
+                options.UserInteraction.LogoutUrl = "/Account/Logout";
+                options.Authentication = new IdentityServer4.Configuration.AuthenticationOptions()
+                {
+                    CookieLifetime = TimeSpan.FromHours(10), // ID server cookie timeout set to 10 hours
+                    CookieSlidingExpiration = true
+                };
+            })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.EnableTokenCleanup = true;
+            })
+            .AddAspNetIdentity<ApplicationUser>();
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+                        
+            services.AddScoped<IProfileService, ProfileService>();
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
