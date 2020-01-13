534acc14b1ae38ad1c285feae772ba88cb515733
JovanBabic <jovan.babic@deblokt.com>
2020-01-13 11:19:59 +0100

    07. IdentityServer4 MFA - TOTP - initial commit

---
+++ 07. IdentityServer4 MFA – TOTP/src/IdentityServer/Services/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            string key = _configuration.GetSection("SendGrid").GetValue<string>("ApiKey");
+            return Execute(key, subject, message, email);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            string fromEmail = _configuration.GetSection("SendGrid").GetValue<string>("FromEmail");
+            string fromName = _configuration.GetSection("SendGrid").GetValue<string>("FromName");
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(fromEmail, fromName),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
